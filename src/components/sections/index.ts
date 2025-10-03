e083dda9dd4731182d5aa77eb7c3f6639296802b
Adrian Demaestri <adriandemaestri@hotmail.com>
2025-10-03 13:17:03 -0300

    feat: agregar secciones de Fecha, Fin, Lugar y Presentacion al componente de Sections

---
+++ src/components/sections/index.ts
@@ -0,0 +1,5 @@
+import Fecha from "./Fecha.astro";
+import Fin from "./Fin.astro";
+import Lugar from "./Lugar.astro";
+import Presentacion from "./Presentacion.astro";
+export { Fecha, Fin, Lugar, Presentacion };
