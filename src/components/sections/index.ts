93fd1bf53d61f1bd10edc8811a216e2812fe78bf
Adrian Demaestri <adriandemaestri@hotmail.com>
2025-10-06 07:35:09 -0300

    feat: renombrar secciones de Contacto, Fecha, Fin, Lugar y Presentación al componente de Sections

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,6 @@
-import Fecha from "./Fecha.astro";
-import Fin from "./Fin.astro";
-import Lugar from "./Lugar.astro";
-import Presentacion from "./Presentacion.astro";
-export { Fecha, Fin, Lugar, Presentacion };
+import Date from "./Date.astro";
+import End from "./End.astro";
+import Location from "./Location.astro";
+import Contact from "./Contact.astro";
+import Welcome from "./Welcome.astro";
+export { Date, End, Location, Contact, Welcome };
